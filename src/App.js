b433582982496966016184fd1a1430a867b91451
Sripathy K <sripathy.k@gmail.com>
2018-12-10 20:03:22 -0800

    update text of App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React now!
+            Learn React now from a feature branch!
           </a>
         </header>
         <p>
